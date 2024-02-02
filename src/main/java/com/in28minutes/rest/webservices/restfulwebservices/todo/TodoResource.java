28d9327193ba298c10207ae9a40e2cd39f6765d7
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-02-01 17:44:45 -0800

    Creating Retrieve Todo and Delete Todo Spring Boot REST API Methods

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,5 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,15 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos")
     public List<Todo> retrieveTodos(@PathVariable String username){
         return todoService.findByUsername(username);
+    }
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo retrieveTodo(@PathVariable String username,@PathVariable int id){
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping ("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodos(@PathVariable String username, @PathVariable int id){
+        todoService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
