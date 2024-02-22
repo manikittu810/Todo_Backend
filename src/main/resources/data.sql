72e82ee2716aafff161f918b8ff91219fc2ec955
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-02-22 14:46:10 -0800

    Full Stack React & Spring Boot with JPA & Hibernate - Todo CRUD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO todo (id, description, done, target_date, username)
+VALUES (10001, 'learn AWS', false, CURRENT_DATE(), 'in28minutes');
+
+INSERT INTO todo (id, description, done, target_date, username)
+VALUES (10002, 'get AWS', false, CURRENT_DATE(), 'in28minutes');
+
+INSERT INTO todo (id, description, done, target_date, username)
+VALUES (10003, 'put AWS', false, CURRENT_DATE(), 'in28minutes');
